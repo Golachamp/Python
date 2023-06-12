4f4581a42921aa73eba6deb95847ea07e2dc256a
Rachana <racjob.2018@gmail.com>
2023-06-12 22:23:55 +0100

    New version

--- pacakages.py
+++ pacakages.py
@@ -1,2 +1,8 @@
 import os
+import shutil
 print(os.getcwd())
+#print(shutil.disk_usage("/"))
+total, used, free = shutil.disk_usage("/")
+print("Total disk space is",total // (2**30))
+print("Total used space is",used // (2**30)) 
+print("Total free space is",free // (2**30))
